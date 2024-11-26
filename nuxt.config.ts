20b1527021f7f58f33bb7653418ef36c1d8d1bf6
Serhan Kılıç <sserhankilic@gmail.com>
2024-11-26 14:36:57 +0300

    updated some files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,18 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
     devtools: { enabled: true },
     modules: [
-      '@nuxt/icon',
-      '@vueuse/nuxt',
-      '@nuxt/ui',
-      '@nuxtjs/tailwindcss',
-      '@nuxtjs/color-mode',
+        '@nuxt/icon',
+        '@vueuse/nuxt',
+        '@nuxt/ui',
+        '@@nuxtjs/tailwindcss',
+        '@nuxtjs/color-mode',
+        '@nuxtjs/google-fonts',
+        [
+            '@nuxtjs/google-fonts',
+            {
+                families: ['Poppins', 'sans-serif'],
+            },
+        ],
     ],
+    css: ['~/assets/css/global.css'],
 })
